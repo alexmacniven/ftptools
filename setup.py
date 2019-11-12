c24b6fd914849161c73a5cc30d469ec40b508bc7
Alex Macniven <macniven.ap@gmail.com>
2019-11-12 13:47:47 +0000

    Bumps Version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/alexmacniven/ftptools"
 EMAIL = "macniven.ap@gmail.com"
 AUTHOR = "Alex Macniven"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "1.0.0-2"
+VERSION = "1.0.0-3"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
